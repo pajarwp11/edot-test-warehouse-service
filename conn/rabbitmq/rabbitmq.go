9a8b4f20da36cfc7750ca141120e930b869e4547
Pajar Wijaya Putra <pajar.putra.aux@flip.id>
2025-02-08 19:49:06 +0700

    add rabbitmq consumer

--- conn/rabbitmq/rabbitmq.go
+++ conn/rabbitmq/rabbitmq.go
@@ -1,17 +1,91 @@
 package rabbitmq
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/rabbitmq/amqp091-go"
 )
 
-var RabbitConn *amqp091.Connection
+type Event struct {
+	Type string                 `json:"type"`
+	Data map[string]interface{} `json:"data"`
+}
+
+var (
+	RabbitConn  *amqp091.Connection
+	exhangeName = "stock_events"
+)
 
 func Connect() {
 	var err error
 	RabbitConn, err = amqp091.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+}
+
+func ConsumeEvents() {
+	ch, err := RabbitConn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		exhangeName,
+		"topic",
+		true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topics := map[string]string{
+		"stock.transfer": "queue_stock_transfer",
+	}
+
+	for routingKey, queueName := range topics {
+		go startConsumer(ch, queueName, routingKey)
+	}
+
+	log.Println("consumer started")
+	select {}
+}
+
+func startConsumer(ch *amqp091.Channel, queueName, routingKey string) {
+	q, err := ch.QueueDeclare(
+		queueName,
+		true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ch.QueueBind(q.Name, routingKey, exhangeName, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for d := range msgs {
+			var event Event
+			json.Unmarshal(d.Body, &event)
+			log.Printf("Received from %s: %+v\n", queueName, event)
+			handleEvent(event)
+		}
+	}()
+}
+
+func handleEvent(event Event) {
+	switch event.Type {
+	case "stock.transfer":
+		fmt.Println("Handling stock reservation:", event.Data)
+	default:
+		fmt.Println("Unknown event:", event.Type)
 	}
 }
